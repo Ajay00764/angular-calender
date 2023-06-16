6ae0567eae585a86b519b47b5df55e7f2bf8840d
Ajay <kharatajay314@gmail.com>
2023-06-16 17:31:49 +0530

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions, EventSourceInput } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calender';
+  
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    height : 600,
+    events: [
+      { title: 'Today ', date: '2023-06-16' },
+      { title: 'Shiv Jayanti', date: '2023-06-02' }
+    ],
+    weekends: true,
+    // dateClick: this.handleDateClick.bind(this)
+    eventClick : this.handleDateClick.bind(this)
+  };
+  eventsPromise!: Promise<EventSourceInput>;
+  handleDateClick(arg:any) {
+    alert('date click! ' + arg.dateStr)
+  }
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+  }
+
 }
